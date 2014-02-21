2352ed80ab1fa96fe2559a53debcaabd0d808e0e
David Nolen <david.nolen@gmail.com>
2014-02-21 15:23:29 -0500

    wip

--- examples/basic/src/core.cljs
+++ examples/basic/src/core.cljs
@@ -1,1 +1,16 @@
-(ns examples.basic.core)
+(ns examples.basic.core
+  (:require [goog.dom :as gdom]
+            [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]
+            [worlds.core :as worlds]))
+
+(def app-state (worlds/world {:text "Hello worlds!"}))
+
+(defn app-view [app owner]
+  (reify
+    IRender
+    (-render [_]
+      (dom/div nil
+        (dom/h2 nil (:text app))))))
+
+(om/root app-view app-state {:target (gdom/getElement "app")})
--- project.clj
+++ project.clj
@@ -3,9 +3,13 @@
   :url "http://github.com/swannodette/worlds"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2173"]
                  [om "0.5.0"]]
+
+  :plugins [[lein-cljsbuild "1.0.2"]]
+
   :cljsbuild {
     :builds [{:id "basic"
               :source-paths ["src" "test"]
